26af2910abb2c1a5ac1e57cf0dcb99292cff3946
Sebastian Benz <sebastian.benz@gmail.com>
2012-02-09 20:50:30 +0100

    examples group don't need an extra headline

--- de.bmw.carit.jnario.spec.ui/src/de/bmw/carit/jnario/spec/ui/doc/SpecHoverProvider.java
+++ de.bmw.carit.jnario.spec.ui/src/de/bmw/carit/jnario/spec/ui/doc/SpecHoverProvider.java
@@ -4,7 +4,6 @@
 package de.bmw.carit.jnario.spec.ui.doc;
 
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.xtext.ui.editor.hover.html.DefaultEObjectHoverProvider;
 import org.eclipse.xtext.xtend2.ui.hover.XtendHoverProvider;
 
 import com.google.inject.Inject;
@@ -20,6 +19,18 @@ public class SpecHoverProvider extends XtendHoverProvider {
 	
 	@Inject
 	private DocGenerator docGenerator;
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.xtext.xbase.ui.hover.XbaseHoverProvider#getFirstLine(org.eclipse.emf.ecore.EObject)
+	 */
+	@Override
+	protected String getFirstLine(EObject o) {
+		if (o instanceof ExampleGroup) {
+			return "";
+		}else{
+			return super.getFirstLine(o);
+		}
+	}
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.xtext.ui.editor.hover.html.DefaultEObjectHoverProvider#getDocumentation(org.eclipse.emf.ecore.EObject)
