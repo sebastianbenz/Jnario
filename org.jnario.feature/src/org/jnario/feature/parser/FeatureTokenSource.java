f70f92966123aebaf64f6140a908ac59676838dd
Sebastian Benz <sebastian.benz@gmail.com>
2012-04-12 18:27:16 +0200

    added licence headers

--- org.jnario.feature/src/org/jnario/feature/parser/FeatureTokenSource.java
+++ org.jnario.feature/src/org/jnario/feature/parser/FeatureTokenSource.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2012 BMW Car IT and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.jnario.feature.parser;
 
 import static org.jnario.feature.parser.antlr.internal.InternalFeatureLexer.RULE_BACKGROUND_TEXT;
--- org.jnario/src/org/jnario/doc/FilePathComputer.xtend
+++
@@ -1,5 +0,0 @@
-package org.jnario.doc
-
-class FilePathComputer {
-	
-}
