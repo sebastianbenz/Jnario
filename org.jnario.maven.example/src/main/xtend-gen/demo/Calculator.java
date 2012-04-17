4104b1ea21336e2c3a4c27f47a563697bf83c162
Sebastian Benz <sebastian.benz@gmail.com>
2012-04-17 21:06:30 +0200

    adds SDK feature with source bundles

--- org.jnario.maven.example/src/main/xtend-gen/demo/Calculator.java
+++ org.jnario.maven.example/src/main/xtend-gen/demo/Calculator.java
@@ -1,19 +1,8 @@
-/*******************************************************************************
- * Copyright (c) 2012 BMW Car IT and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
-
 package demo;
-
-import org.eclipse.xtext.xbase.lib.IntegerExtensions;
 
 @SuppressWarnings("all")
 public class Calculator {
   public int add(final int a, final int b) {
-    int _operator_plus = IntegerExtensions.operator_plus(a, b);
-    return _operator_plus;
+    return (a + b);
   }
 }
