daa413f7d67a42a75ee122a3eaf2770adfa88921
Sebastian Benz <sebastian.benz@gmail.com>
2012-04-23 23:52:56 +0200

    fixes location for step references

--- org.jnario.feature/src/org/jnario/feature/resource/FeatureLocationInFileProvider.java
+++ org.jnario.feature/src/org/jnario/feature/resource/FeatureLocationInFileProvider.java
@@ -1,5 +1,6 @@
 package org.jnario.feature.resource;
 
+import static org.eclipse.xtext.nodemodel.util.NodeModelUtils.findNodesForFeature;
 import static org.jnario.util.Strings.trimWhitespaceAtEnd;
 
 import java.util.Collections;
@@ -8,23 +9,28 @@ import java.util.List;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtend.core.resource.XtendLocationInFileProvider;
 import org.eclipse.xtext.nodemodel.INode;
-import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.util.ITextRegion;
 import org.eclipse.xtext.util.TextRegion;
 import org.jnario.feature.feature.FeaturePackage;
-import org.jnario.feature.feature.StepImplementation;
+import org.jnario.feature.feature.Step;
+import org.jnario.feature.feature.StepReference;
 
 public class FeatureLocationInFileProvider extends XtendLocationInFileProvider {
 
 	protected ITextRegion getTextRegion(EObject obj, boolean isSignificant) {
-		if(isSignificant && obj instanceof StepImplementation){
-			return getSignificantTextRegionStep((StepImplementation) obj);
+		if(isSignificant && obj instanceof Step){
+			return getSignificantTextRegionStep((Step) obj);
 		}
 		return super.getTextRegion(obj, isSignificant);
 	}
 	
-	private ITextRegion getSignificantTextRegionStep(StepImplementation element) {
-		List<INode> nodes = NodeModelUtils.findNodesForFeature(element, FeaturePackage.Literals.STEP__NAME);
+	private ITextRegion getSignificantTextRegionStep(Step element) {
+		List<INode> nodes;
+		if (element instanceof StepReference) {
+			nodes = findNodesForFeature(element, FeaturePackage.Literals.STEP_REFERENCE__REFERENCE);
+		}else{
+			nodes = findNodesForFeature(element, FeaturePackage.Literals.STEP__NAME);
+		}
 		if (!nodes.isEmpty()) {
 			ITextRegion result = ITextRegion.EMPTY_REGION;
 			for (int i=0; i<nodes.size(); i++) {
