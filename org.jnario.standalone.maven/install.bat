1c1c5fbededf892bf51d0358c4b43a1f36dce449
Sebastian Benz <sebastian.benz@gmail.com>
2012-06-17 01:11:47 +0200

    update maven dependencies

--- org.jnario.standalone.maven/install.bat
+++
@@ -1,8 +0,0 @@
-@rem ***************************************************************************
-@rem Copyright (c) 2012 BMW Car IT and others.
-@rem All rights reserved. This program and the accompanying materials
-@rem are made available under the terms of the Eclipse Public License v1.0
-@rem which accompanies this distribution, and is available at
-@rem http://www.eclipse.org/legal/epl-v10.html
-@rem ***************************************************************************
-mvn install:install-file -Dfile=output/org.jnario.standalone.maven-0.1.0.jar -DgroupId=org.jnario -DartifactId=org.jnario.standalone.maven -Dversion=0.1.0 -Dpackaging=jar 
