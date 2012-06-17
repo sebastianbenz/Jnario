46b0c5e5c6abdcd2f93e600785678a41d35686dd
Sebastian Benz <sebastian.benz@gmail.com>
2012-06-17 12:35:11 +0200

    fix build after tyco update

--- org.jnario.lib.tests/src/org/jnario/lib/tests/NameProviderTest.java
+++ org.jnario.lib.tests/src/org/jnario/lib/tests/NameProviderTest.java
@@ -7,11 +7,11 @@
  *******************************************************************************/
 package org.jnario.lib.tests;
 
+import static org.eclipse.xtext.util.Strings.convertFromJavaString;
 import static org.junit.Assert.assertEquals;
 
 import org.jnario.runner.NameProvider;
 import org.jnario.runner.Named;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runners.model.TestClass;
 
@@ -19,13 +19,12 @@ public class NameProviderTest {
 
 	@Named("string with special char \\u00E4")
 	private static class Example{
-		
 	}
 	
 	@Test
 	public void convertsFromJavaString() throws Exception {
 		String actual = new NameProvider().nameOf(new TestClass(Example.class));
-		assertEquals("string with special char ä", actual);
+		assertEquals("string with special char " + convertFromJavaString("\\u00E4", true), actual);
 	}
 	
 }
