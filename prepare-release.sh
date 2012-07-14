0a7847b9906194a29cdb3ed796c72e9a1ca515a2
Sebastian Benz <sebastian.benz@gmail.com>
2012-07-14 16:56:10 +0200

    improve release scripts
    - deploy target to update snapshot site and maven repo
    - updated all version identifiers & dependencies
    - improve release scripts

---
+++ prepare-release.sh
@@ -0,0 +1,55 @@
+set -e
+timestamp=$(date +%Y%m%d%H%M)
+echo "preparing release"
+echo "release version:"
+read version
+echo "next version:"
+read nextversion
+echo "Releasing $version"
+
+#pull the latest state from the repository
+pull --rebase
+
+#prepare update site
+cd ../jnario-gh-pages
+git pull
+cd ../jnario
+
+#run all tests
+mvn clean verify
+
+#create release branch
+git branch rb-$version
+
+#change the releng/update-site/category.xml file. Replace <version>.qualifier by <version+1>
+sed -i "s/$version/$nextversion/g" releng/org.jnario.updatesite/category.xml
+
+#update master to the next -SNAPSHOT version. 
+
+#update tycho managed plugins
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$nextversion-SNAPSHOT
+
+#update maven managed projects
+for project in "org.jnario.standalone.maven" "jnario-maven-plugin" "org.jnario.lib.maven" 
+do
+cd plugins/$project
+mvn versions:set -DnewVersion=$nextversion-SNAPSHOT -DgenerateBackupPoms=false
+cd ../..
+done
+
+#commit and push to remote
+git commit -s -a -m "new version $nextversion-SNAPSHOT"
+git push
+
+#checkout the release branch
+git checkout rb-$version
+
+#pick release BUILDQUALIFIER (eg, 20100924-1107) and update release branch to the release version
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=<version>.$timestamp
+  
+#commit and tag your changes
+git commit -s -a -m "new release $version"
+git tag v$version
+git push --tags  origin master
+
+exit 1
---
+++ release.sh
@@ -0,0 +1,30 @@
+set -e
+
+echo "release version:"
+read version
+
+#make the release build
+mvn clean install -Dmaven.test.skip=true -P release 
+
+#publish update site
+cd ../jnario-gh-pages
+git add --all
+git commit -a -m "new release $version"
+cd ../jnario
+
+# upload to maven central 
+URL="https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+REPO="sonatype-nexus-staging"
+for project in "org.jnario.standalone.maven" "jnario-maven-plugin" "org.jnario.lib.maven" 
+do
+cd plugins/$project
+mvn versions:set -DnewVersion=$version -DgenerateBackupPoms=false
+mvn gpg:sign-and-deploy-file -Dpackaging=jar -Durl=$URL -DrepositoryId=$REPO -DpomFile=pom.xml -Dfile=target/$project-$version.jar
+mvn gpg:sign-and-deploy-file -Dpackaging=jar -Durl=$URL -DrepositoryId=$REPO -DpomFile=pom.xml -Dfile=target/$project-$version-sources.jar -Dclassifier=sources
+mvn gpg:sign-and-deploy-file -Dpackaging=jar -Durl=$URL -DrepositoryId=$REPO -DpomFile=pom.xml -Dfile=target/$project-$version-javadoc.jar -Dclassifier=javadoc
+cd ../..
+done
+
+echo "Successfully staged maven artifacts. Manually close & release from https://oss.sonatype.org/index.html#stagingRepositories"
+
+exit 1 
