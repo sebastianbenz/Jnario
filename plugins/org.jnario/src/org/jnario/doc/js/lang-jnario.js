ce4ca2ef52f783b49887cac47ffaf5d52447d202
Sebastian Benz <sebastian.benz@gmail.com>
2012-09-12 11:20:10 +0200

    improve syntax highlighting for suites

--- plugins/org.jnario/src/org/jnario/doc/js/lang-jnario.js
+++ plugins/org.jnario/src/org/jnario/doc/js/lang-jnario.js
@@ -8,7 +8,7 @@
 
 /**
  * @fileoverview
- * Registers a language handler Jnario based.
+ * Registers a language handler for Jnario.
  *
  * @author Sebastian Zarnekow
  * @author Sebastian Benz
@@ -74,48 +74,6 @@ PR['registerLangHandler'](
 	           ]),
 	    ['feature']);
 
-	    
-PR['registerLangHandler'](
-		   PR['createSimpleLexer'](
-		        [
-		         [PR['PR_PLAIN'],       /^[\t\n\r \xA0]+/, null, '\t\n\r \xA0'],
-		         [PR['PR_STRING'],      /^((\'\'\'|\u00BB)([\s\S]*?)(\'\'\'|\u00AB))/]
-		         [PR['PR_PUNCTUATION'], /^[!#%&()*+,\-:;<=>?@\[\\\]^{|}~]+/, null,
-		          '!#%&()*+,-:;<=>?@[\\]^{|}~']
-		        ],
-		        [
-		         // A symbol literal is a single quote followed by an identifier with no
-		         // single quote following
-		         // A character literal has single quotes on either side
-		         [PR['PR_STRING'],      /^'(?:[^\r\n\\']|\\(?:'|[^\r\n']+))'/],
-		         [PR['PR_LITERAL'],     /^(?:(?:0(?:[0-7]+|X[0-9A-F]+))L?|(?:(?:0|[1-9][0-9]*)(?:(?:\.[0-9]+)?(?:E[+\-]?[0-9]+)?F?|L?))|\\.[0-9]+(?:E[+\-]?[0-9]+)?F?)/i],
-		         [PR['PR_PUNCTUATION'], /^(?:\.+|\/)/]
-		        ]),
-		    ['none']);
-		    
-PR['registerLangHandler'](
-	   PR['createSimpleLexer'](
-	        [
-	         [PR['PR_PLAIN'],       /^[\t\n\r \xA0]+/, null, '\t\n\r \xA0'],
-	         [PR['PR_STRING'],      /^((\'\'\'|\u00BB)([\s\S]*?)(\'\'\'|\u00AB))/]
-	         [PR['PR_PUNCTUATION'], /^[!#%&()*+,\-:;<=>?@\[\\\]^{|}~]+/, null,
-	          '!#%&()*+,-:;<=>?@[\\]^{|}~']
-	        ],
-	        [
-	         [PR['PR_TAG'],     	/(?:#+[^\r\n]+)\b/],
-	         [PR['PR_KEYWORD'],     /^(?:package|import)\b/],
-	         [PR['PR_LITERAL'],     /^(?:(?:0(?:[0-7]+|X[0-9A-F]+))L?|(?:(?:0|[1-9][0-9]*)(?:(?:\.[0-9]+)?(?:E[+\-]?[0-9]+)?F?|L?))|\\.[0-9]+(?:E[+\-]?[0-9]+)?F?)/i],
-	         [PR['PR_PUNCTUATION'], /^(?:\.+|\/)/]
-	        ]),
-	    ['suite']);
-
-
-/**
- * @fileoverview
- * Registers a language handler for Xtend.
- *
- * @author Sebastian Zarnekow
- */
 
 PR['registerLangHandler'](
     PR['createSimpleLexer'](
@@ -125,7 +83,43 @@ PR['registerLangHandler'](
          [PR['PR_PUNCTUATION'], /^[!#%&()*+,\-:;<=>?@\[\\\]^{|}~]+/, null,
           '!#%&()*+,-:;<=>?@[\\]^{|}~'],
 
-//         , null]
+//	    	         , null]
+        ],
+        [
+         // A double or single quoted, possibly multi-line, string.
+         [PR['PR_STRING'],        /^(?:"(?:[^\"\\]|\\.)*"|'(?!\'\')(?:[^\'\\]|\\.)*')/],
+         // A symbol literal is a single quote followed by an identifier with no
+         // single quote following
+         // A character literal has single quotes on either side
+         [PR['PR_STRING'],      /^'(?:[^\r\n\\']|\\(?:'|[^\r\n']+))'/],
+         [PR['PR_LITERAL'],     /^'[a-zA-Z_$][\w$]*(?!['$\w])/],
+         [PR['PR_KEYWORD'],     /^(?:import|package)\b/],
+         [PR['PR_LITERAL'],     /^(?:(?:0(?:[0-7]+|X[0-9A-F]+))L?|(?:(?:0|[1-9][0-9]*)(?:(?:\.[0-9]+)?(?:E[+\-]?[0-9]+)?F?|L?))|\\.[0-9]+(?:E[+\-]?[0-9]+)?F?)/i],
+         [PR['PR_PLAIN'],       /^[$a-zA-Z_][\w$]*/],
+         [PR['PR_COMMENT'],     /^\/(?:\/.*|\*(?:\/|\**[^*/])*(?:\*+\/?)?)/],
+         [PR['PR_PUNCTUATION'], /^(?:\.+|\/)/],
+         [PR['PR_KEYWORD'], 	/^#(?:##(?:[^#\\]|#(?!##))*(?:###|$)|.*)/, null, '#'],
+         [PR['PR_STRING'],      /^((\'\'\'|\u00BB)([\s\S]*?)(\'\'\'|\u00AB))/]
+
+        ]),
+    ['suite']);
+
+    /**
+     * @fileoverview
+     * Registers a language handler for Xtend.
+     *
+     * @author Sebastian Zarnekow
+     */
+
+PR['registerLangHandler'](
+    PR['createSimpleLexer'](
+        [
+          // Whitespace
+         [PR['PR_PLAIN'],       /^[\t\n\r \xA0]+/, null, '\t\n\r \xA0'],
+         [PR['PR_PUNCTUATION'], /^[!#%&()*+,\-:;<=>?@\[\\\]^{|}~]+/, null,
+          '!#%&()*+,-:;<=>?@[\\]^{|}~'],
+
+//             , null]
         ],
         [
          // A double or single quoted, possibly multi-line, string.
@@ -147,3 +141,29 @@ PR['registerLangHandler'](
 
         ]),
     ['xtend']);
+    
+PR['registerLangHandler'](
+	    PR['createSimpleLexer'](
+	        [
+	          // Whitespace
+	         [PR['PR_PLAIN'],       /^[\t\n\r \xA0]+/, null, '\t\n\r \xA0'],
+	         [PR['PR_PUNCTUATION'], /^[!#%&()*+,\-:;<=>?@\[\\\]^{|}~]+/, null,
+	          '!#%&()*+,-:;<=>?@[\\]^{|}~'],
+
+//    	             , null]
+	        ],
+	        [
+	         [PR['PR_STRING'],        /^(?:"(?:[^\"\\]|\\.)*"|'(?!\'\')(?:[^\'\\]|\\.)*')/],
+	         [PR['PR_STRING'],      /^'(?:[^\r\n\\']|\\(?:'|[^\r\n']+))'/],
+	         [PR['PR_LITERAL'],     /^'[a-zA-Z_$][\w$]*(?!['$\w])/],
+	         [PR['PR_LITERAL'],     /^(?:(?:0(?:[0-7]+|X[0-9A-F]+))L?|(?:(?:0|[1-9][0-9]*)(?:(?:\.[0-9]+)?(?:E[+\-]?[0-9]+)?F?|L?))|\\.[0-9]+(?:E[+\-]?[0-9]+)?F?)/i],
+	         // Treat upper camel case identifiers as types.
+	         [PR['PR_PLAIN'],       /^[$a-zA-Z_][\w$]*/],
+	         [PR['PR_COMMENT'],     /^\/(?:\/.*|\*(?:\/|\**[^*/])*(?:\*+\/?)?)/],
+	         [PR['PR_PUNCTUATION'], /^(?:\.+|\/)/],
+	         [PR['PR_STRING'],      /^((\'\'\'|\u00BB)([\s\S]*?)(\'\'\'|\u00AB))/]
+
+	        ]),
+	    ['none']);
+		    
+
