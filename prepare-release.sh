db22d188784abc629a8b38ed61db0ed695f8ba43
Sebastian Benz <sebastian.benz@gmail.com>
2012-09-18 10:03:50 +0200

    new release 0.2.1
    Signed-off-by: Sebastian Benz <sebastian.benz@gmail.com>

--- prepare-release.sh
+++ prepare-release.sh
@@ -3,28 +3,7 @@ timestamp=$(date +%Y%m%d%H%M)
 echo "preparing release"
 echo "release version:"
 read version
-echo "next version:"
-read nextversion
 echo "Releasing $version"
-
-
-#update tycho managed plugins
-mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$nextversion-SNAPSHOT
-
-#update maven managed projects
-for project in "org.jnario.standalone.maven" "jnario-maven-plugin" "org.jnario.lib.maven" 
-do
-cd plugins/$project
-mvn versions:set -DnewVersion=$nextversion-SNAPSHOT -DgenerateBackupPoms=false
-cd ../..
-done
-
-#commit and push to remote
-git commit -s -a -m "new version $nextversion-SNAPSHOT"
-git push
-
-#checkout the release branch
-git checkout rb-$version
 
 #pick release BUILDQUALIFIER (eg, 20100924-1107) and update release branch to the release version
 mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$version.$timestamp
