4a9d99489b5c2d797737f10095c78b0a74a3c60a
Sebastian Benz <sebastian.benz@gmail.com>
2012-10-23 18:21:15 +0200

    regenerate doc

--- examples/org.jnario.maven.example/src/main/java/demo/Calculator.xtend
+++ examples/org.jnario.maven.example/src/main/java/demo/Calculator.xtend
@@ -10,7 +10,7 @@ package demo
 class Calculator {
 	
 	def add(int a, int b){
-		return a + b + 2
+		return a + b 
 	}
 	
 }  
--- examples/org.jnario.maven.example/src/main/xtend-gen/demo/Calculator.java
+++
@@ -1,8 +0,0 @@
-package demo;
-
-@SuppressWarnings("all")
-public class Calculator {
-  public int add(final int a, final int b) {
-    return (a + b);
-  }
-}
