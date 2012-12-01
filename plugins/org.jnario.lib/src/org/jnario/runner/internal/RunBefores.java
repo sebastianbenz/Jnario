a0b806c0df25bb5641353fa1dd9987bdbef2c610
Sebastian Benz <sebastian.benz@gmail.com>
2012-12-02 00:17:14 +0100

    clean dependencies
    - add explicit plugin dependency to hamcrest
    - remove access to internal JUnit classes

---
+++ plugins/org.jnario.lib/src/org/jnario/runner/internal/RunBefores.java
@@ -0,0 +1,30 @@
+package org.jnario.runner.internal;
+
+import java.util.List;
+
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.Statement;
+
+/*
+ * Copied from JUnit
+ */
+public class RunBefores extends Statement {
+	private final Statement fNext;
+
+	private final Object fTarget;
+
+	private final List<FrameworkMethod> fBefores;
+
+	public RunBefores(Statement next, List<FrameworkMethod> befores, Object target) {
+		fNext= next;
+		fBefores= befores;
+		fTarget= target;
+	}
+
+	@Override
+	public void evaluate() throws Throwable {
+		for (FrameworkMethod before : fBefores)
+			before.invokeExplosively(fTarget);
+		fNext.evaluate();
+	}
+}
