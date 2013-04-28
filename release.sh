0562c1c038911ac22142ea1cc6417ba77f098c8f
Sebastian Benz <sebastian.benz@gmail.com>
2013-04-28 16:51:40 +0200

    made release build

--- release.sh
+++ release.sh
@@ -6,19 +6,19 @@ read version
 #make the release build
 mvn clean install -Dmaven.test.skip=true -P release 
 
-#publish update site
+publish update site
 cd ../jnario-gh-pages
 git add --all
 git commit -a -m "new release $version"
 git push origin gh-pages
 cd ../jnario
 
-# upload to maven central 
+## upload to maven central 
 URL="https://oss.sonatype.org/service/local/staging/deploy/maven2/"
 REPO="sonatype-nexus-staging"
 
 #todo: remove all snapshot dependencies
-for project in "org.jnario.standalone.maven" "jnario-maven-plugin" "org.jnario.lib.maven" "jnario-maven-report-plugin"
+for project in "org.jnario.standalone.maven" "jnario-maven-plugin" "org.jnario.lib.maven"
 do
 cd plugins/$project
 mvn versions:set -DnewVersion=$version -DgenerateBackupPoms=false
@@ -28,8 +28,21 @@ mvn gpg:sign-and-deploy-file -Dpackaging=jar -Durl=$URL -DrepositoryId=$REPO -Dp
 mvn gpg:sign-and-deploy-file -Dpackaging=jar -Durl=$URL -DrepositoryId=$REPO -DpomFile=pom.xml -Dfile=target/$project-$version-javadoc.jar -Dclassifier=javadoc
 cd ../..
 done
-mvn gpg:sign-and-deploy-file -Dpackaging=jar -Durl=$URL -DrepositoryId=$REPO -DpomFile=pom.xml -Dfile=target/jnario-maven-archetype-$version.jar
 
+cd plugins/jnario-maven-report-plugin
+mvn versions:set -DnewVersion=$version -DgenerateBackupPoms=false
+mvn clean install
+mvn gpg:sign-and-deploy-file -Dpackaging=jar -Durl=$URL -DrepositoryId=$REPO -DpomFile=pom.xml -Dfile=target/report-$version.jar
+mvn gpg:sign-and-deploy-file -Dpackaging=jar -Durl=$URL -DrepositoryId=$REPO -DpomFile=pom.xml -Dfile=target/report-$version-sources.jar -Dclassifier=sources
+mvn gpg:sign-and-deploy-file -Dpackaging=jar -Durl=$URL -DrepositoryId=$REPO -DpomFile=pom.xml -Dfile=target/report-$version-javadoc.jar -Dclassifier=javadoc
+cd ../..
+
+cd plugins/jnario-maven-archetype
+mvn versions:set -DnewVersion=$version -DgenerateBackupPoms=false
+mvn clean install
+mvn gpg:sign-and-deploy-file -Dpackaging=jar -Durl=$URL -DrepositoryId=$REPO -DpomFile=pom.xml -Dfile=target/jnario-archetype-$version.jar
+mvn gpg:sign-and-deploy-file -Dpackaging=jar -Durl=$URL -DrepositoryId=$REPO -DpomFile=pom.xml -Dfile=target/jnario-archetype-$version-sources.jar -Dclassifier=sources
+cd ../..
 echo "Successfully staged maven artifacts. Manually close & release via https://oss.sonatype.org/index.html#stagingRepositories"
 
 exit 1 
