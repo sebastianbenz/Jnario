39616c21301f3408927606d7e6181ab8e2fab3c7
Sebastian Benz <sebastian.benz@gmail.com>
2013-07-17 23:23:22 +0200

    new release 0.5.0
    Signed-off-by: Sebastian Benz <sebastian.benz@gmail.com>

--- prepare-release.sh
+++ prepare-release.sh
@@ -8,45 +8,45 @@ read nextversion
 echo "Releasing $version"
 
 #pull the latest state from the repository
-git pull --rebase
+#git pull --rebase
 
 #prepare update site
-cd ../jnario-gh-pages
-git pull origin gh-pages
-cd ../jnario
+#cd ../jnario-gh-pages
+#git pull origin gh-pages
+#cd ../jnario
 
 #run all tests
-mvn clean verify
+#mvn clean verify
 
 #create release branch
-git branch rb-$version
+#git branch rb-$version
 
 #change the releng/update-site/category.xml file. Replace <version>.qualifier by <version+1>
-sed -i '' 's/$version/$nextversion/g' releng/org.jnario.updatesite/category.xml
-sed -i '' 's/$version/$nextversion/g' releng/org.jnario.updatesite/site.xml
+#sed -i '' 's/$version/$nextversion/g' releng/org.jnario.updatesite/category.xml
+#sed -i '' 's/$version/$nextversion/g' releng/org.jnario.updatesite/site.xml
 
 #update master to the next -SNAPSHOT version. 
 
 #update tycho managed plugins
-mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$nextversion-SNAPSHOT
+#mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$nextversion-SNAPSHOT
 
 #update maven managed projects
-for project in "org.jnario.standalone.maven" "jnario-maven-plugin" "org.jnario.lib.maven" "jnario-maven-report-plugin" "jnario-maven-archetype"
-do
-cd plugins/$project
-mvn versions:set -DnewVersion=$nextversion-SNAPSHOT -DgenerateBackupPoms=false
-cd ../..
-done
+#for project in "org.jnario.standalone.maven" "jnario-maven-plugin" "org.jnario.lib.maven" "jnario-maven-report-plugin" "jnario-maven-archetype"
+#do
+#cd plugins/$project
+#mvn versions:set -DnewVersion=$nextversion-SNAPSHOT -DgenerateBackupPoms=false
+#cd ../..
+#done
 
 #commit and push to remote
-git commit -s -a -m "new version $nextversion-SNAPSHOT"
-git push
+#git commit -s -a -m "new version $nextversion-SNAPSHOT"
+#git push
 
 #checkout the release branch
-git checkout rb-$version
+#git checkout rb-$version
 
 #pick release BUILDQUALIFIER (eg, 20100924-1107) and update release branch to the release version
-mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$version.$timestamp
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:0.17.0:set-version -DnewVersion=$version.$timestamp
   
 #commit and tag your changes
 git commit -s -a -m "new release $version"
