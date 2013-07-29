e7e0e8ae6f5b8fadf304fa38ec06fa26774133f3
Sebastian Benz <sebastian.benz@gmail.com>
2013-07-29 20:12:54 +0200

    fix compile error

--- plugins/org.jnario.feature.ui/src/org/jnario/feature/ui/quickfix/FeatureCreateMemberQuickfixes.java
+++ plugins/org.jnario.feature.ui/src/org/jnario/feature/ui/quickfix/FeatureCreateMemberQuickfixes.java
@@ -5,17 +5,17 @@ import java.util.List;
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.xtend.ide.quickfix.CreateMemberQuickfixes;
 import org.eclipse.xtext.common.types.JvmDeclaredType;
-import org.eclipse.xtext.common.types.JvmTypeReference;
 import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
 import org.eclipse.xtext.validation.Issue;
 import org.eclipse.xtext.xbase.XAbstractFeatureCall;
+import org.eclipse.xtext.xbase.typesystem.references.LightweightTypeReference;
 
 public class FeatureCreateMemberQuickfixes extends CreateMemberQuickfixes{
 
 	@Override
 	protected void newMethodQuickfix(JvmDeclaredType containerType,
-			String name, @Nullable JvmTypeReference returnType,
-			List<JvmTypeReference> parameterTypes, boolean isStatic,
+			String name, @Nullable LightweightTypeReference returnType,
+			List<LightweightTypeReference> parameterTypes, boolean isStatic,
 			boolean isAbstract, boolean isExtension, boolean isLocal,
 			XAbstractFeatureCall call, Issue issue,
 			IssueResolutionAcceptor issueResolutionAcceptor) {
