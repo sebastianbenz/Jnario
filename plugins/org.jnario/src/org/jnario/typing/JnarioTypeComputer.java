3e31c626ee558f1de5a40faec7efd594687a8e80
Sebastian Benz <sebastian.benz@gmail.com>
2014-01-11 22:14:32 +0100

    make should be null candidate selection deterministic2

--- plugins/org.jnario/src/org/jnario/typing/JnarioTypeComputer.java
+++ plugins/org.jnario/src/org/jnario/typing/JnarioTypeComputer.java
@@ -1,12 +1,20 @@
 package org.jnario.typing;
 
+
 import static org.jnario.jvmmodel.DoubleArrowSupport.isDoubleArrow;
 
 import java.util.List;
+import java.util.concurrent.locks.AbstractOwnableSynchronizer;
 
 import org.eclipse.xtend.core.typesystem.XtendTypeComputer;
+import org.eclipse.xtext.common.types.JvmFormalParameter;
 import org.eclipse.xtext.common.types.JvmOperation;
+import org.eclipse.xtext.common.types.JvmPrimitiveType;
+import org.eclipse.xtext.common.types.JvmTypeReference;
+import org.eclipse.xtext.common.types.access.impl.Primitives;
 import org.eclipse.xtext.common.types.util.TypeReferences;
+import org.eclipse.xtext.diagnostics.AbstractDiagnostic;
+import org.eclipse.xtext.util.IAcceptor;
 import org.eclipse.xtext.xbase.XAbstractFeatureCall;
 import org.eclipse.xtext.xbase.XBinaryOperation;
 import org.eclipse.xtext.xbase.XExpression;
@@ -15,6 +23,7 @@ import org.eclipse.xtext.xbase.typesystem.computation.IFeatureLinkingCandidate;
 import org.eclipse.xtext.xbase.typesystem.computation.ILinkingCandidate;
 import org.eclipse.xtext.xbase.typesystem.computation.ITypeComputationState;
 import org.eclipse.xtext.xbase.typesystem.conformance.ConformanceHint;
+import org.eclipse.xtext.xbase.typesystem.internal.AbstractPendingLinkingCandidate;
 import org.eclipse.xtext.xbase.typesystem.internal.FeatureLinkingCandidate;
 import org.eclipse.xtext.xbase.typesystem.internal.ITypeLiteralLinkingCandidate;
 import org.eclipse.xtext.xbase.typesystem.util.CommonTypeComputationServices;
@@ -60,18 +69,29 @@ public class JnarioTypeComputer extends XtendTypeComputer {
 			ITypeComputationState state) {
 		if (isShouldBeNull(featureCall) || isDoubleArrowNull(featureCall)) {
 			List<? extends IFeatureLinkingCandidate> candidates = state.getLinkingCandidates(featureCall);
-			if(candidates.isEmpty()){
-				return;
-			}
 			for (IFeatureLinkingCandidate candidate : candidates) {
-				if(candidate.getTypeArguments().isEmpty()){
-					candidate.applyToComputationState();
-					return;
+				if (candidate.getFeature() instanceof JvmOperation) {
+					JvmOperation operation = (JvmOperation) candidate.getFeature();
+					if(canHandleNullArg(operation)){
+						candidate.applyToComputationState();
+						return;
+					}
 				}
 			}
 		} else {
 			super._computeTypes(featureCall, state);
 		}
+	}
+
+	private boolean canHandleNullArg(JvmOperation operation) {
+		if(operation.getParameters().size() != 2){
+			return false;
+		}
+		JvmTypeReference parameterType = operation.getParameters().get(1).getParameterType();
+		if(parameterType == null){
+			return false;
+		}
+		return !(parameterType.getType() instanceof JvmPrimitiveType);
 	}
 
 	private boolean isDoubleArrowNull(final XAbstractFeatureCall featureCall) {
