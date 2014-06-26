63aa35d055b53e0d2208935d221e8071d502dc45
Sebastian Benz <sebastian.benz@gmail.com>
2014-06-26 23:54:37 +0200

    regenerate doc & spec

--- examples/org.jnario.examples/xtend-gen/diverse/StackEmptySpec.java
+++ examples/org.jnario.examples/xtend-gen/diverse/StackEmptySpec.java
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2012 BMW Car IT and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- */
 package diverse;
 
 import diverse.StackSpec;
@@ -15,7 +8,6 @@ import org.jnario.lib.Should;
 import org.jnario.runner.ExampleGroupRunner;
 import org.jnario.runner.Named;
 import org.jnario.runner.Order;
-import org.jnario.runner.Subject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -23,31 +15,30 @@ import org.junit.runner.RunWith;
 @RunWith(ExampleGroupRunner.class)
 @SuppressWarnings("all")
 public class StackEmptySpec extends StackSpec {
-  @Subject
-  public Stack subject;
+  final Stack<String> emptyStack = new Stack<String>();
   
   @Test
-  @Named("subject.empty[] should be true")
+  @Named("emptyStack.size should be 0")
   @Order(1)
-  public void _subjectEmptyShouldBeTrue() throws Exception {
-    boolean _empty = this.subject.empty();
-    boolean _should_be = Should.<Boolean>should_be(Boolean.valueOf(_empty), true);
-    Assert.assertTrue("\nExpected subject.empty() should be true but"
-     + "\n     subject.empty() is " + new org.hamcrest.StringDescription().appendValue(Boolean.valueOf(_empty)).toString()
-     + "\n     subject is " + new org.hamcrest.StringDescription().appendValue(this.subject).toString() + "\n", _should_be);
+  public void _emptyStackSizeShouldBe0() throws Exception {
+    int _size = this.emptyStack.size();
+    boolean _should_be = Should.<Integer>should_be(Integer.valueOf(_size), Integer.valueOf(0));
+    Assert.assertTrue("\nExpected emptyStack.size should be 0 but"
+     + "\n     emptyStack.size is " + new org.hamcrest.StringDescription().appendValue(Integer.valueOf(_size)).toString()
+     + "\n     emptyStack is " + new org.hamcrest.StringDescription().appendValue(this.emptyStack).toString() + "\n", _should_be);
     
   }
   
   @Test
-  @Named("subject.pop[] throws EmptyStackException")
+  @Named("emptyStack.pop throws EmptyStackException")
   @Order(2)
-  public void _subjectPopThrowsEmptyStackException() throws Exception {
+  public void _emptyStackPopThrowsEmptyStackException() throws Exception {
     boolean expectedException = false;
     String message = "";
     try{
-      this.subject.pop();
-      message = "Expected " + EmptyStackException.class.getName() + " for \n     subject.pop()\n with:"
-       + "\n     subject is " + new org.hamcrest.StringDescription().appendValue(this.subject).toString();
+      this.emptyStack.pop();
+      message = "Expected " + EmptyStackException.class.getName() + " for \n     emptyStack.pop\n with:"
+       + "\n     emptyStack is " + new org.hamcrest.StringDescription().appendValue(this.emptyStack).toString();
     }catch(EmptyStackException e){
       expectedException = true;
     }
